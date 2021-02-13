4937f7d0e22f85e3c1676dd97a9b92d1af9705e3
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-13 09:47:42 +0300

    добавлены опции и парсер опций
    токен бота можно передать в аргументе "bot-token" командной строки при запуске приложения

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from cardProvider import CardProvider
+from options import Options
+from optionsParser import OptionsParser
+from telegramBot import TelegramBot
+
+# получаем настройки приложения
+options = OptionsParser.parse()
 
 card_provider = CardProvider()
 card = card_provider.get_random("Политика")
 tags = card_provider.get_all_tags()
+
+# создаем и запускаем телеграм бота
+bot = TelegramBot(options.telegram_bot_token)
+bot.go()
+
 pass
---
+++ options.py
@@ -0,0 +1,3 @@
+class Options:
+    def __init__(self, telegram_bot_token: str):
+        self.telegram_bot_token = telegram_bot_token
