077a1e6d47b2ba1c6bf199bc8f2a14e841d64ac8
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-13 15:55:49 +0300

    удалил ненужный кусок кода + удалил комменарии

--- main.py
+++ main.py
@@ -3,13 +3,7 @@ from options import Options
 from optionsParser import OptionsParser
 from telegramBot import TelegramBot
 
-# получаем настройки приложения
 options = OptionsParser.parse()
-
 card_provider = CardProvider()
-card = card_provider.get_random("Политика")
-tags = card_provider.get_all_tags()
-
-# создаем и запускаем телеграм бота
 bot = TelegramBot(options.telegram_bot_token, card_provider)
 bot.go()
