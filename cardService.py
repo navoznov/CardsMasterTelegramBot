e577d4d34220be79d22fc41c1a633909838bf53e
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-15 15:07:47 +0300

    поправил импорты + убрал хинты

--- cardService.py
+++ cardService.py
@@ -1,15 +1,16 @@
 from cardRepository import CardRepository
-
+from card import Card
+import random
 
 class CardService:
     def __init__(self, card_repository: CardRepository):
         self.__card_repository = card_repository
         random.seed()
 
-    def get_all(self) -> list(Card):
+    def get_all(self):
         return self.__card_repository.get_all()
 
-        def get_by_tag(self, tag: str) -> list(Card):
+    def get_by_tag(self, tag: str):
         return self.__card_repository.get_by_tag(tag)
 
     def get_random(self, tag: str = None) -> Card:
@@ -20,7 +21,7 @@ class CardService:
         card = random.choice(cards)
         return card
 
-    def get_all_tags(self) -> list(str):
+    def get_all_tags(self):
         cards = self.__card_repository.get_all()
         all_tags = [card.tag for card in cards]
         return list(set(all_tags))
