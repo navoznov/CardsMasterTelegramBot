055a733d0c7a31e0d7094d1b99572058f7117232
Navoznov Ivan <ivan.navoznov@gmail.com>
2021-02-15 14:37:54 +0300

    избавился от поля EMPTY_ID

--- card.py
+++ card.py
@@ -1,7 +1,5 @@
 class Card:
-    EMPTY_ID = -1
-
-    def __init__(self, text, tag, author, country, id: int = EMPTY_ID):
+    def __init__(self, text, tag, author, country, id: int = -1):
         self.id = id
         self.text = text
         self.tag = tag
@@ -9,8 +7,8 @@ class Card:
         self.country = country
 
     @staticmethod
-    def from_dict(card_dict: dict) -> Card:
-        id = card_dict.get("id", EMPTY_ID)
+    def from_dict(card_dict: dict):
+        id = card_dict.get("id", -1)
         author = card_dict.get("author", None)
         country = card_dict.get("country", None)
         card = Card(card_dict["text"], card_dict["tag"], author, country, id)
